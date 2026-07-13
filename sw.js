a6c56725c76388c3828b669cc87020235cfc0cf1
WhackAMole Deploy <whack-a-mole@local.deploy>
2026-07-13 19:39:16 +0800

    Use transparent-background custom mole sprite

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 简易离线缓存：添加到主屏幕后，无网也能打开玩 */
-const CACHE = "whack-a-mole-v7-custom-mole";
+/* 简易离线缓存：添加到主屏幕后，无网也能打开�?*/
+const CACHE = "whack-a-mole-v8-transparent-mole";
 const ASSETS = [
   "./",
   "./index.html",
@@ -35,3 +35,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
