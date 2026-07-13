b91e9b64053f79152b9c56afa0833d3d8a7a66e3
WhackAMole Deploy <whack-a-mole@local.deploy>
2026-07-13 10:25:26 +0800

    Deploy whack-a-mole game for GitHub Pages

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* 简易离线缓存：添加到主屏幕后，无网也能打开玩 */
+const CACHE = "whack-a-mole-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-180.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
